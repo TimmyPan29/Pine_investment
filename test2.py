03f86d0fef64a9ecd56815f67fce2909a4e73ab9
Timmy29 <timmy29@29Macmini.local>
2024-02-28 16:13:10 +0800

    fix k bar index problem

--- test2.py
+++ test2.py
@@ -1,16 +1,22 @@
 //@version=5
+indicator("hr,week sbd sbu", shorttitle="SB", overlay=true)
 
-//**變數
-//  *注意刷新sbd sbu後各項變數要初始化
+////**自定義參數
+//  *
 //  *//
 
-indicator("hr,week sbd sbu", shorttitle="SB", overlay=true)
+var int Number_bar = 7 
+
+////**變數
+//  *注意刷新sbd sbu後各項變數要初始化
+//  *//
 var int barCount = 0
 var int slope1 = 0
 var int slope2 = 0
 var int state = na
 var bool isbreakSBU = na
 var bool isbreakSBD = na
+var bool isbreak = na
 var float close_SBU= na
 var float close_SBD= na
 var float Buff_close1 = na
@@ -18,10 +24,13 @@ var float Buff_close2 = na
 var float Buff_close3 = na
 var float Buff_key1 = na
 var float Buff_key2 = na
+var line myLine = na
+var label mylabel  = na
+var line myLine2 = na
+var label mylabel2  = na
 barCount := barCount+1
 
-
-//**初始條件
+////**初始條件
 //  *先處理前三個點,初始化SBD SBU
 //  *// 
 
@@ -54,53 +63,78 @@ if (barCount == 3)
         if(slope1!=slope2)
             Buff_key1:=Buff_close2 //第一次轉折點出現
 
-//**向上突破 向下突破 待在空間中 三種情況
+////**向上突破 向下突破 待在空間中 三種情況
 //  *三種情況來寫判別式，且從第四根bar開始算，先滿足破，再滿足是否轉折點
 //  *state2 : Buff_close2有突破且isbreak有跳起來
 //  *state3 : state2的反
 //  *state4 : end並畫圖
-//  *破的轉點一律叫key2，缺少突破的控制訊號<
+//  *破的轉點一律叫key2，缺少突破的控制訊號
+//  *支撐被破之後 SBU要馬上跟上，而在嚴格遞減的情況下，此時的SBD不可以長出來
 //  *//   
 
 if(state==1 and barCount>3) //從第四點開始
     isbreakSBU := close>close_SBU? true : false
     isbreakSBD := close<close_SBD? true : false
+    if(isbreakSBU or isbreakSBD)
+        isbreak := true
     Buff_close1 := Buff_close2
     Buff_close2 := Buff_close3
     Buff_close3 := close
     slope1 := slope2
     slope2 := Buff_close3-Buff_close2>0? 1:-1
-    if((isbreakSBU and (Buff_close2>close_SBU)) or (isbreakSBD and (Buff_close2<close_SBD)))
+    if(isbreak)
         state := 2
     else
         state := 3
-    if(barstate.islast)
-        state := 4
+
 if(state==2)
     if(slope1!=slope2) 
         Buff_key2 := Buff_close2
         if(Buff_key2>Buff_key1) //代表上破
             close_SBU := Buff_key2
             close_SBD := Buff_key1
-            isbreakSBU := false
+            isbreakSBU := na
         else //下破
             close_SBD := Buff_key2
             close_SBU := Buff_key1
-            isbreakSBD := false
+            isbreakSBD := na
         Buff_key1 := Buff_key2
         Buff_key2 := na
+        isbreak := na
     else
+        if(isbreakSBU)
+            close_SBD := Buff_key1
+            close_SBU := na
+        if(isbreakSBD)
+            close_SBU := Buff_key1
+            close_SBD := na
         Buff_key2 := na
     state := 1
+    if(barCount==Number_bar)                               
+        state := 4
+
 if(state==3)
     if(slope1!=slope2)
         Buff_key1 := Buff_close2
     else
         Buff_key1 := Buff_key1
     state := 1
+    if(barCount==Number_bar)                                
+        state := 4
+
 if(state==4)
-    line.new(x1=barCount-100, y1=close_SBU, x2=barCount + 100, y2=close_SBU, width=2, color=color.purple)
-    line.new(x1=barCount-100, y1=close_SBD, x2=barCount + 100, y2=close_SBD, width=2, color=color.purple)
-    line.new(x1=barCount-100, y1=Buff_key2, x2=barCount + 100, y2=Buff_key2, width=2, color=color.yellow)
+    if(na(mylabel)==false)
+        label.delete(mylabel)
+    mylabel := label.new(bar_index, low, text="k bar: " + str.tostring(bar_index+1), color=color.green) 
+    if (na(myLine) == false)
+        line.delete(myLine)
+    myLine := line.new(x1=bar_index, y1=low, x2=bar_index, y2=high, width=1, color=color.red, style=line.style_solid)
+
+    line.new(x1=bar_index-100, y1=close_SBU, x2=bar_index +100, y2=close_SBU, width=2, color=color.purple)
+    line.new(x1=bar_index-100, y1=close_SBD, x2=bar_index +100, y2=close_SBD, width=2, color=color.purple)
+    line.new(x1=1-100, y1=Buff_close2, x2=1 + 100, y2=Buff_close2, width=2, color=color.yellow)
+    //line.new(x1=1-100, y1=Buff_key1, x2=1 + 100, y2=Buff_key1, width=2, color=color.orange)
+    //line.new(x1=1-100, y1=Buff_close3, x2=1 + 100, y2=Buff_close3, width=2, color=color.black)
+    state := na
 
     
--- test3.py
+++ test3.py
@@ -11,6 +11,7 @@ var int slope2 = 0
 var int state = na
 var bool isbreakSBU = na
 var bool isbreakSBD = na
+var bool isbreak = na
 var float close_SBU= na
 var float close_SBD= na
 var float Buff_close1 = na
@@ -18,6 +19,10 @@ var float Buff_close2 = na
 var float Buff_close3 = na
 var float Buff_key1 = na
 var float Buff_key2 = na
+var line myLine = na
+var label mylabel  = na
+var line myLine2 = na
+var label mylabel2  = na
 barCount := barCount+1
 
 
@@ -64,19 +69,19 @@ if (barCount == 3)
 
 if(state==1 and barCount>3) //從第四點開始
     isbreakSBU := close>close_SBU? true : false
-    isbreakSBU := Buff_close2>close_SBU? true : false
     isbreakSBD := close<close_SBD? true : false
-    isbreakSBD := Buff_close2<close_SBD? true : false
+    if(isbreakSBD or isbreakSBU)
+        isbreak := true
     Buff_close1 := Buff_close2
     Buff_close2 := Buff_close3
     Buff_close3 := close
     slope1 := slope2
     slope2 := Buff_close3-Buff_close2>0? 1:-1
-    if((isbreakSBU and (Buff_close2>close_SBU)) or (isbreakSBD and (Buff_close2<close_SBD)))
+    if(isbreak)
         state := 2
     else
         state := 3
-    if(barCount==6)
+    if(barCount==10)
         state := 4
 if(state==2)
     if(slope1!=slope2) 
@@ -84,13 +89,14 @@ if(state==2)
         if(Buff_key2>Buff_key1) //代表上破
             close_SBU := Buff_key2
             close_SBD := Buff_key1
-            isbreakSBU := false
+            isbreakSBU := na
         else //下破
             close_SBD := Buff_key2
             close_SBU := Buff_key1
-            isbreakSBD := false
+            isbreakSBD := na
         Buff_key1 := Buff_key2
         Buff_key2 := na
+        isbreak := na
     else
         Buff_key2 := na
     state := 1
@@ -101,8 +107,25 @@ if(state==3)
         Buff_key1 := Buff_key1
     state := 1
 if(state==4)
-    line.new(x1=1-100, y1=close_SBU, x2=1 + 100, y2=close_SBU, width=2, color=color.purple)
-    line.new(x1=1-100, y1=close_SBD, x2=1 + 100, y2=close_SBD, width=2, color=color.purple)
-    line.new(x1=1-100, y1=Buff_close2, x2=1 + 100, y2=Buff_close2, width=2, color=color.yellow)
-    line.new(x1=1-100, y1=Buff_key1, x2=1 + 100, y2=Buff_key1, width=2, color=color.orange)
-    line.new(x1=1-100, y1=Buff_key2, x2=1 + 100, y2=Buff_key2, width=2, color=color.black)
+    if(na(mylabel)==false)
+        label.delete(mylabel)
+    mylabel := label.new(bar_index, high, text="k bar: " + str.tostring(bar_index+1), color=color.green) 
+    if (na(myLine) == false)
+        line.delete(myLine)
+    myLine := line.new(x1=bar_index, y1=low, x2=bar_index, y2=high, width=1, color=color.red, style=line.style_solid)
+
+    if(na(mylabel2)==false)
+        label.delete(mylabel2)
+    mylabel2 := label.new(bar_index-1, high, text="因為系統判定下一根還沒收盤\n所以判斷的地方落在這點 k bar" + str.tostring(bar_index), color=color.green) 
+    if (na(myLine2) == false)
+        line.delete(myLine2)
+    myLine2 := line.new(x1=bar_index-1, y1=low, x2=bar_index-1, y2=high, width=1, color=color.red, style=line.style_solid)
+
+    line.new(x1=bar_index-100, y1=close_SBU, x2=bar_index +100, y2=close_SBU, width=2, color=color.purple)
+    line.new(x1=bar_index-100, y1=close_SBD, x2=bar_index +100, y2=close_SBD, width=2, color=color.purple)
+    //line.new(x1=1-100, y1=Buff_close2, x2=1 + 100, y2=Buff_close2, width=2, color=color.yellow)
+    //line.new(x1=1-100, y1=Buff_key1, x2=1 + 100, y2=Buff_key1, width=2, color=color.orange)
+    //line.new(x1=1-100, y1=Buff_close3, x2=1 + 100, y2=Buff_close3, width=2, color=color.black)
+    state := na
+
+    
