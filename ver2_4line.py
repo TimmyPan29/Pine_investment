e319c8dde6bf52de4c1408c643c2ee2882a9fc19
Timmy29 <timmy29@29Macmini.local>
2024-03-15 05:20:23 +0800

    level

--- ver2_4line.py
+++ ver2_4line.py
@@ -25,13 +25,13 @@ indicator("hr,week sbd sbu", shorttitle="SB", overlay=true)
 //time variable
 currentperiod = str.tonumber(timeframe.period)
 currentperiod_div4 = currentperiod/4
-currentperiod_div4_str = str.tostring(str.tonumber(currentperiod)/4)
+currentperiod_div4_str = str.tostring(currentperiod_div4)
 currentYear = year(time)
 currentMon = month(time)
 currentDay = dayofmonth(time) //必須對準交易所的第幾天
 currentHr = hour(time) // hour 和 minute是對準交易所得時差
 currentMin = mimute(time)
-var int summerhour = 6 
+var int HrMin2Min = currentHr*60+currentMin 
 //**
 
 //common variable
@@ -40,17 +40,18 @@ var int NumShift = 0
 var int state = na
 var int nextstate = na
 var int statelevel = na
-var int count = na 
+var int count = 0 
 var int level = na 
 var int BarCount = 0
 var float Quotient = na
 var int Remainder = na
-var bool GoGoFlag = na
+var bool GoGoFlag = false
 var float start_low = na
 var float start_high = na
 var bool SizeFlag = na
+var string TICKERID = syminfo.tickerid
 var array<float> arrayclose = array.new(currentperiod_div4,0)
-var array<float> arraybuff =  array.new(20,0)
+var array<float> arraybuff = array.new(0)
 //**
 
 //figure variable 
@@ -62,13 +63,18 @@ var label Label_SBU_1over4 = na
 var label Label_SBD_1over4 = na
 //**
 
-//local parameter
-var int RESET = 1
-var int NOSKY = 2
-var int NOGRD = 3
-var int SURRD = 4
-var int HOLD = 5
-var int PLOT = 6
+//local parameter and constant
+var const int RESET = 1
+var const int NOSKY = 2
+var const int NOGRD = 3
+var const int SURRD = 4
+var const int HOLD = 5
+var const int PLOT = 6
+var const int DAY2MINUTE = 1440
+var const int LEVEL1 = 1
+var const int LEVEL2 = 2
+var const int LEVEL3 = 3
+var const int LEVEL4 = 4 
 //**
 
 //test variable
@@ -83,12 +89,36 @@ var int slope1 = na
 var int slope2 = na
 var int index_key1_1over4 = na
 var int index_key2_1over4 = na
+var int index_key1_2over4 = na
+var int index_key2_2over4 = na
+var int index_key1_3over4 = na
+var int index_key2_3over4 = na
+var int index_key1_4over4 = na
+var int index_key2_4over4 = na
 var int index_SBU_1over4 = 0
 var int index_SBD_1over4 = 0
+var int index_SBU_2over4 = 0
+var int index_SBD_2over4 = 0
+var int index_SBU_3over4 = 0
+var int index_SBD_3over4 = 0
+var int index_SBU_4over4 = 0
+var int index_SBD_4over4 = 0
 var float close_SBU_1over4= 0
 var float close_SBD_1over4= 0
+var float close_SBU_2over4= 0
+var float close_SBD_2over4= 0
+var float close_SBU_3over4= 0
+var float close_SBD_3over4= 0
+var float close_SBU_4over4= 0
+var float close_SBD_4over4= 0
 var float Buff_key1_1over4 = na
 var float Buff_key2_1over4 = na
+var float Buff_key1_2over4 = na
+var float Buff_key2_2over4 = na
+var float Buff_key1_3over4 = na
+var float Buff_key2_3over4 = na
+var float Buff_key1_4over4 = na
+var float Buff_key2_4over4 = na
 //**
 
 ////**state ctrl
@@ -97,7 +127,13 @@ var float Buff_key2_1over4 = na
 BarCount := BarCount+1
 ReqClose := request.security_lower_tf(syminfo.tickerid,currentperiod_div4,close)
 SizeFlag := array.size(ReqClose)==4? true : false
-if(BarCount != 0 and currentYear==2023 and currentMon==8 and currentDay==7 and currentHr==5) //BarCount = bar_index+1, example BarCount=1,bar_index=0
+Quotient := math.floor(float(DAY2MINUTE)/currentperiod)
+Remainder := DAY2MINUTE%currentperiod
+if(HrMin2Min==1024 and str.contains(TICKERID,"OANDA"))
+    GoGoFlag = true
+if(HrMin2Min==0 and str.contains(TICKERID,"EIGHTCAP"))
+    GoGoFlag = true
+if(GoGoFlag) //BarCount = bar_index+1, example BarCount=1,bar_index=0
     state := SURRD
 else
     state := nextstate
