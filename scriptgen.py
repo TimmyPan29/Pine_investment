0e2bc89750cdad4885d991680553a87c5f18bf2b
TimmyPan29 <q0vm3xu44400@gmail.com>
2024-06-13 04:08:12 +0800

    30linex success

--- scriptgen.py
+++ scriptgen.py
@@ -2,9 +2,12 @@
 output = ""
 
 for i in range(1, 31):
-    output += f"""htf{i}.trace.trace_c_color   := input.color(color.new(color.black, 50), "SkinHTF{i}    ", inline='htflevel{i}', group="trace")
-htf{i}.trace.trace_c_style   := input.string('⎯⎯⎯', '', options = ['⎯⎯⎯', '----', '····'], inline='htflevel{i}', group="trace")
-htf{i}.trace.trace_c_size    := input.int(2, '', options = [1,2,3,4], inline='htflevel{i}', group="trace")
+    output += f"""if  htf{i}.settings.show and helper.ValidTimeframe(htf{i}.settings.htf) 
+    htf{i}.Monitor().BOSJudge()
+    if barstate.isrealtime or barstate.islast
+        htf{i}.Monitor_Est().BOSJudge()
+        plotdata(htf{i}, offset, delta)
+    cnt +=1
 """
 print(output)
 
